8a842d645e80a3e40b764e0a1786bfd931265251
Maria Larionova <maria.larionova@helsinki.fi>
2019-02-11 13:31:38 +0200

    osa3 lisaa

---
+++ build/precache-manifest.71000849985e689f5400b277ccd94fa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e339d77572f5b7a69a37",
+    "url": "/static/js/main.e339d775.chunk.js"
+  },
+  {
+    "revision": "23feea3aedfdbea1a84a",
+    "url": "/static/js/1.23feea3a.chunk.js"
+  },
+  {
+    "revision": "e339d77572f5b7a69a37",
+    "url": "/static/css/main.47b69d3b.chunk.css"
+  },
+  {
+    "revision": "069af16c31dc319f0afa212f5eb8d092",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.c0cd9a8daf1669ff4e37f93754039c43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "72784ad63f11d1becee1",
+    "url": "/static/js/main.72784ad6.chunk.js"
+  },
+  {
+    "revision": "23feea3aedfdbea1a84a",
+    "url": "/static/js/1.23feea3a.chunk.js"
+  },
+  {
+    "revision": "72784ad63f11d1becee1",
+    "url": "/static/css/main.47b69d3b.chunk.css"
+  },
+  {
+    "revision": "902d4eb2438e9925dcd6538f5ad3bc5d",
+    "url": "/index.html"
+  }
+];
